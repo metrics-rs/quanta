0e6d83d760c43e43b68d8475ce294d0d586101c2
Toby Lawrence <tobz@users.noreply.github.com>
2023-12-31 11:01:11 -0500

    fix: switch to `clock_gettime(3)` for macOS/iOS for monotonic clock (#97)
    This fixes an issue where on M1 (and presumably M2/M3) hardware,
    the usage of mach_absolute_time() resulted in undermeasuring the
    passage of time when compared to std::time::Instant. Now we read
    from the monotonic clock (using clock_gettime, but conceptually
    the same as calling mach_continuous_time()) which properly tracks
    the passage of time regardless of device sleep, frequency, etc.

--- src/clocks/monotonic/mod.rs
+++ src/clocks/monotonic/mod.rs
@@ -1,8 +1,3 @@
-#[cfg(any(target_os = "macos", target_os = "ios"))]
-mod macos;
-#[cfg(any(target_os = "macos", target_os = "ios"))]
-pub use self::macos::Monotonic;
-
 #[cfg(target_os = "windows")]
 mod windows;
 #[cfg(target_os = "windows")]
@@ -13,17 +8,7 @@ mod wasm;
 #[cfg(target_arch = "wasm32")]
 pub use self::wasm::Monotonic;
 
-#[cfg(not(any(
-    target_os = "macos",
-    target_os = "ios",
-    target_os = "windows",
-    target_arch = "wasm32"
-)))]
+#[cfg(not(any(target_os = "windows", target_arch = "wasm32")))]
 mod unix;
-#[cfg(not(any(
-    target_os = "macos",
-    target_os = "ios",
-    target_os = "windows",
-    target_arch = "wasm32"
-)))]
+#[cfg(not(any(target_os = "windows", target_arch = "wasm32")))]
 pub use self::unix::Monotonic;
