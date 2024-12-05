cf90e5a00b5ce8eff69ec9fdc91ffdcba67dd974
officeyutong <yt.xyxx@gmail.com>
2024-12-05 23:50:17 +0800

    fix: use thread_local for performance instance

--- src/clocks/monotonic/wasm_browser.rs
+++ src/clocks/monotonic/wasm_browser.rs
@@ -1,4 +1,5 @@
-/// Since threads (WebWorkers) in browser don't shared the same memory space, so no objects would have a chance to be sended to other threads, thus it's safe to implement Send for objects here
+use std::cell::OnceCell;
+
 use web_sys::{
     js_sys::Reflect,
     wasm_bindgen::{JsCast, JsValue},
@@ -9,45 +10,29 @@ const WASM_MISSING_GLOBAL_THIS_PERF: &str = "failed to find `globalThis.performa
 const WASM_UNABLE_TO_CAST_PERF: &str =
     "Unable to cast `globalThis.performance` to Performance type";
 
-struct PerformanceClassWrapper(Performance);
-
-unsafe impl Send for PerformanceClassWrapper {}
-unsafe impl Sync for PerformanceClassWrapper {}
-
-static GLOBAL_PERFORMANCE_INSTANCE: std::sync::OnceLock<PerformanceClassWrapper> =
-    std::sync::OnceLock::new();
-
-#[derive(Clone, Copy, Debug)]
-pub struct Monotonic {
-    performance: &'static Performance,
+thread_local! {
+    static GLOBAL_PERFORMANCE_INSTANCE: OnceCell<Performance> = OnceCell::new();
 }
 
-unsafe impl Send for Monotonic {}
-unsafe impl Sync for Monotonic {}
+#[derive(Clone, Copy, Debug, Default)]
+pub struct Monotonic {
+    _default: (),
+}
 
-impl Default for Monotonic {
-    fn default() -> Self {
-        let performance = GLOBAL_PERFORMANCE_INSTANCE.get_or_init(|| {
-            PerformanceClassWrapper(
+impl Monotonic {
+    pub fn now(&self) -> u64 {
+        let now = GLOBAL_PERFORMANCE_INSTANCE.with(|value| {
+            let performance_instance = value.get_or_init(|| {
                 Reflect::get(
                     &web_sys::js_sys::global(),
                     &JsValue::from_str("performance"),
                 )
                 .expect(WASM_MISSING_GLOBAL_THIS_PERF)
                 .dyn_into::<Performance>()
-                .expect(WASM_UNABLE_TO_CAST_PERF),
-            )
+                .expect(WASM_UNABLE_TO_CAST_PERF)
+            });
+            performance_instance.now()
         });
-
-        Self {
-            performance: &performance.0,
-        }
-    }
-}
-
-impl Monotonic {
-    pub fn now(&self) -> u64 {
-        let now = self.performance.now();
         // `performance.now()` returns the time in milliseconds.
         return f64::trunc(now * 1_000_000.0) as u64;
     }
